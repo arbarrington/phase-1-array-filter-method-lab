7ebcada00401300add115e9075f5e1c4ced8045a
Austin Barrington <arbarrington12@gmail.com>
2022-08-10 15:32:21 -0600

    commit tree 305f7986cbe775aff5e0e387ba85a05dc2b70fe1

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Code your solution here
+function findMatching(driver, name) {
+    return driver.filter(
+        (mayMatch) => mayMatch.toLowerCase() === name.toLowerCase())
+    }
+
+function fuzzyMatch(driver, name) {
+    return driver.filter(
+        (mayMatch) => mayMatch.toLowerCase().indexOf(name.toLowerCase()) === 0
+    );
+}
+
+function matchName(driver, name) {
+    return driver.filter((mayMatch) => mayMatch.name === name
+    )
+}
